e116e7539514224c057fc84bfd3c7b97229eaf5d
SArpnt <36542932+SArpnt@users.noreply.github.com>
2021-12-07 21:27:01 -0500

    file io

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+#![allow(non_snake_case)]
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
 fn main() {
-	println!("Hello, world!");
+	let inPath = Path::new("./tests/testCode.non");
+	let mut inFile = match File::open(&inPath) {
+		Err(why) => panic!("couldn't open {}: {}", inPath.display(), why),
+		Ok(inFile) => inFile,
+	};
+	let mut inString = String::new();
+	match inFile.read_to_string(&mut inString) {
+		Err(why) => panic!("couldn't read {}: {}", inPath.display(), why),
+		Ok(_) => print!("{} contains:\n{}\n", inPath.display(), inString),
+	}
+	let inputString = inString;
+
+
+	let outputString = "testOutput";
+
+	let outPath = Path::new("./output.c");
+	let mut outFile = match File::create(&outPath) {
+		Err(why) => panic!("couldn't create {}: {}", outPath.display(), why),
+		Ok(outFile) => outFile,
+	};
+	match outFile.write_all(outputString.as_bytes()) {
+		Err(why) => panic!("couldn't write to {}: {}", outPath.display(), why),
+		Ok(_) => println!("successfully wrote to {}", outPath.display()),
+	}
 }
